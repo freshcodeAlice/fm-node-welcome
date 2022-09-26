ce293dea6e197d939d47c73f1719c814058909b8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-26 11:57:17 +0300

    fileSystem module

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 'use strict';
-const {MyMath} = require('./MyMath');
-const sum = MyMath.sum(2,2);
 
-/*
-require(path);
+const fs = require('fs').promises;
 
-HOW REQUIRE WORKS:
-
-resolving -> loading -> wrappening -> evaluation -> caching
-
-RESOLVING:
-    1) Core modules
-    2) File 
-        *.js | *.json
-    3) Directory
-        3.1) package.json -> "main"
-        3.2) index.js | index.json
-    4) node_modules
-    5) throw new Error();
-*/
+fs.readFile('./text.txt','utf-8')
+            .then(content => {
+                const newContent = `OLD content: ${content} and NEW content: 'Again hello'`;
+               fs.writeFile('./newfile.txt', newContent, 'utf-8'); //writeFile - перезаписує
+              //  fs.appendFile('./newfile.txt', 'AND APPEND', 'utf-8'); //append додає в кінець файлу
+            });
