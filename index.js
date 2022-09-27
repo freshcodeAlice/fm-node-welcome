54e372b2966b7b9674056d27822aa47e7fe909ad
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-27 09:50:19 +0300

    Npm init

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const fs = require('fs').promises;
-
-fs.readFile('./text.txt','utf-8')
-            .then(content => {
-                const newContent = `OLD content: ${content} and NEW content: 'Again hello'`;
-               fs.writeFile('./newfile.txt', newContent, 'utf-8'); //writeFile - перезаписує
-              //  fs.appendFile('./newfile.txt', 'AND APPEND', 'utf-8'); //append додає в кінець файлу
-            });
+console.log('hello, world');
--- utils.js
+++
@@ -1,9 +0,0 @@
-const sayHello = (name = 'Anonym') => {
-    console.log(`Hello, ${name}`)
-}
-
-const sayBye = (name = 'Anonym') => {
-    console.log(`Bye-bye, ${name}`)
-}
-
-exports.utils = {sayHello, sayBye};
